ef8a2b7b03e06701a2a07320f01ec2ecd7e75aac
Asli2022 <ashleyaden4u@hotmail.com>
2023-12-05 16:31:25 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -27,6 +27,29 @@ data "aws_ami" "latest_amazon_linux" {
   }
 }
 
+resource "aws_iam_role" "ssm_role" {
+  name = "ssm-ec2-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "ssm.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
 resource "aws_iam_instance_profile" "ssm_instance_profile" {
   name = "ssm-instance-profile"
   role = aws_iam_role.ssm_role.name
