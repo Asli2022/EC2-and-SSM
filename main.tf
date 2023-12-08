fd2ad8b35ecd19a1e0a43d505e808ced9006934f
Asli2022 <ashleyaden4u@hotmail.com>
2023-12-08 16:17:46 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,60 @@
+resource "aws_iam_role" "EC2_SSM_Role" {
+  name = "EC2_SSM_Role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+resource "aws_iam_role_policy_attachment" "EC2_SSM_Policy" {
+  role       = aws_iam_role.EC2_SSM_Role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_instance_profile" "ec2_profile" {
+  name = "ec2_profile"
+  role = aws_iam_role.EC2_SSM_Role.name
+}
+data "aws_ami" "latest_amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+resource "aws_instance" "web-server" {
+  ami                  = data.aws_ami.latest_amazon_linux.id
+  instance_type        = var.instance_type
+  security_groups      = [aws_security_group.web-server_sg.name]
+  iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
+
+
+  user_data = <<-EOF
+#!/bin/bash
+yum update -y
+yum install -y httpd
+systemctl start httpd
+systemctl enable httpd
+echo "<h1>Welcome to Tech with Aden </h1>" > /var/www/html/index.html 
+  EOF
+
+  tags = {
+    Name = "web_instance"
+  }
+}
+
 resource "aws_security_group" "web-server_sg" {
   name        = var.security_group_name
   description = var.security_group_description
@@ -14,65 +71,5 @@ resource "aws_security_group" "web-server_sg" {
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = var.security_group_egress_cidr_blocks
-  }
-}
-
-data "aws_ami" "latest_amazon_linux" {
-  most_recent = true
-  owners      = ["amazon"]
-
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-  }
-}
-
-resource "aws_iam_role" "ssm_role" {
-  name = "ssm-ec2-role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Action = "sts:AssumeRole",
-        Effect = "Allow",
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      },
-      {
-        Action = "sts:AssumeRole",
-        Effect = "Allow",
-        Principal = {
-          Service = "ssm.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
-
-resource "aws_iam_instance_profile" "ssm_instance_profile" {
-  name = "ssm-instance-profile"
-  role = aws_iam_role.ssm_role.name
-}
-
-resource "aws_instance" "web-server" {
-  ami                  = data.aws_ami.latest_amazon_linux.id
-  instance_type        = var.instance_type
-  security_groups      = [aws_security_group.web-server_sg.name]
-  iam_instance_profile = aws_iam_instance_profile.ssm_instance_profile.name
-
-  user_data = <<-EOF
-    #!/bin/bash 
-    sudo su
-    yum update -y
-    yum install -y https://s3.us-east-2.amazonaws.com/amazon-ssm-us-east-2/latest/linux_amd64/amazon-ssm-agent.rpm
-    yum install httpd -y
-    systemctl start httpd
-    systemctl enable httpd
-    echo "<html><h1> Welcome to Tech with Asli Aden ! </h1></html>" >> /var/www/html/index.html       
-  EOF
-
-  tags = {
-    Name = "web_instance"
   }
 }
